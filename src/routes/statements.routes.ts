80e24890487871d0574e628a1ad3411aceb5f2b5
Eduardo Kamo <eduardokamoz@gmail.com>
2025-12-12 07:29:53 -0300

    feat: add balance route

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import { CreateStatementController } from '../modules/accounts/useCases/createStatement/CreateStatementController'
+import { GetBalanceController } from '../modules/accounts/useCases/getBalance/GetBalanceController'
 import { ensureAuthenticated } from '../shared/infra/http/middlewares/ensureAuthenticated'
 
 const statementsRoutes = Router()
 const createStatementController = new CreateStatementController()
+const getBalanceController = new GetBalanceController()
 
 statementsRoutes.use(ensureAuthenticated)
 
+statementsRoutes.get('/balance', getBalanceController.handle)
 statementsRoutes.post('/deposit', createStatementController.handle)
-
 statementsRoutes.post('/withdraw', createStatementController.handle)
 
 export { statementsRoutes }
